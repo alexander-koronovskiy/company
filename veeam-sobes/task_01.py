d6ea2f07003765df86ed76121b4ff9a590c8b4f5
alexander <alexander.koronovskiy@gmail.com>
2021-07-03 19:32:47 +0400

    before copying 1

--- veeam-sobes/task_01.py
+++ veeam-sobes/task_01.py
@@ -6,4 +6,4 @@ for type_tag in root.findall("file"):
     src_path = type_tag.get("source_path")
     destination = type_tag.get("destination_path")
     f_name = type_tag.get("file_name")
-    print(src_path + "/" + f_name, destination)
+    print(src_path + "\\" + f_name, destination)
