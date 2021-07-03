d94f99a88d9461977098ad00ab47c16fb8e3d7cf
alexander <alexander.koronovskiy@gmail.com>
2021-07-03 20:57:07 +0400

    access problems

--- veeam-sobes/task_01.py
+++ veeam-sobes/task_01.py
@@ -1,3 +1,12 @@
+# -*- coding: cp1251 -*-
+"""
+Реализовать программу, осуществляющую копирование файлов
+в соответствии с конфигурационным файлом.
+Конфигурационный файл должен иметь формат xml.
+Для каждого файла в конфигурационном файле должно быть указано его имя,
+исходный путь и путь, по которому файл требуется скопировать.
+"""
+
 import shutil
 import xml.etree.ElementTree as ET
 
@@ -8,7 +17,7 @@ def copying(config, source_path, destination_path, file_name):
         src_path = type_tag.get(source_path)
         destination = type_tag.get(destination_path)
         f_name = type_tag.get(file_name)
-        return src_path + "\\" + f_name, destination
+        shutil.copy(src_path + "\\" + f_name, destination)
 
 
-print(copying("config.xml", "source_path", "destination_path", "file_name"))
+copying("config.xml", "source_path", "destination_path", "file_name")
