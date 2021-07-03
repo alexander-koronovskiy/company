f06e36c3bad38441adc5d9c4c52f93a39479a05d
alexander <alexander.koronovskiy@gmail.com>
2021-07-03 20:01:17 +0400

    copying without rights

--- veeam-sobes/task_01.py
+++ veeam-sobes/task_01.py
@@ -1,9 +1,14 @@
 import shutil
 import xml.etree.ElementTree as ET
 
-root = ET.parse("config.xml").getroot()
-for type_tag in root.findall("file"):
-    src_path = type_tag.get("source_path")
-    destination = type_tag.get("destination_path")
-    f_name = type_tag.get("file_name")
-    print(src_path + "\\" + f_name, destination)
+
+def copying(config, source_path, destination_path, file_name):
+    root = ET.parse(config).getroot()
+    for type_tag in root.findall("file"):
+        src_path = type_tag.get(source_path)
+        destination = type_tag.get(destination_path)
+        f_name = type_tag.get(file_name)
+        return src_path + "\\" + f_name, destination
+
+
+print(copying("config.xml", "source_path", "destination_path", "file_name"))
