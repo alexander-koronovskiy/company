182e1eb568b100abc105689b291b8fd56248af27
alexander <alexander.koronovskiy@gmail.com>
2021-07-03 19:26:10 +0400

    before copying

--- veeam-sobes/task_01.py
+++ veeam-sobes/task_01.py
@@ -1,11 +1,9 @@
+import shutil
 import xml.etree.ElementTree as ET
 
-data = """<?xml version="1.0" encoding="UTF-8"?>
-<config>
-    <file />
-    <file />
-</config>
-"""
-myroot = ET.fromstring(data)
-for x in myroot:
-    print(str(x.tag))
+root = ET.parse("config.xml").getroot()
+for type_tag in root.findall("file"):
+    src_path = type_tag.get("source_path")
+    destination = type_tag.get("destination_path")
+    f_name = type_tag.get("file_name")
+    print(src_path + "/" + f_name, destination)
