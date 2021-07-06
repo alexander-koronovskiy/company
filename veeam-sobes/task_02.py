596ec1a3f01abc97ab595aef6213e341ba312e28
alexander <alexander.koronovskiy@gmail.com>
2021-07-06 18:58:19 +0400

    not formatted code

--- veeam-sobes/task_02.py
+++ veeam-sobes/task_02.py
@@ -10,24 +10,27 @@ import hashlib
 columns = ["name", "algorithm", "hash_sum"]
 files = []
 
-# считать названия файлов, алгоритмов, хэш суммы
+# read
 with open("sum_file.csv") as config:
     for row in [line.split() for line in config]:
         files.append(dict(zip(columns, row)))
 
-# по названию файла найти, проверить хэш, вывести итог
+# check hash sum
 for file in files:
     status = "NOT FOUND"
     content = ""
     try:
         with open(file["name"]) as f:
-            content += f.read()  # file buffer
-            if int(hashlib.sha1(content.encode("utf-8")).hexdigest(), 16) == int(file["hash_sum"], 16):
-                status = "OK"
+            content += f.read()
+            if file["algorithm"] in hashlib.algorithms_available:
+                if int(hashlib.sha1(content.encode("utf-8")).hexdigest(), 16) == int(
+                    file["hash_sum"], 16
+                ):
+                    status = "OK"
+                else:
+                    status = "FAIL"
             else:
                 status = "FAIL"
             print(file["name"], status)
     except FileNotFoundError:
         print(file["name"], status)
-
-# токенайзер для методов hashlib
